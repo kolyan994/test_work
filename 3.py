f68234c4fee7384e8d5dca36e4bff41fcf2ecafa
Николай <130489187+kolyan994@users.noreply.github.com>
2024-01-24 13:14:19 +0300

    Create 3.py
    v1.0

---
+++ 3.py
@@ -0,0 +1,17 @@
+import asyncio
+
+
+async def send_message(message):
+    reader, writer = await asyncio.open_connection('127.0.0.1', 8000)
+    writer.write(message.encode())
+    await writer.drain()
+    print(f'Отправлено: {message}')
+    data = await reader.read(100)
+    print(f'Принято: {data.decode()}')
+    writer.close()
+    await writer.wait_closed()
+    print('Соединение закрыто')
+
+if __name__ == '__main__':
+    message = 'Сообщение на сервер'
+    asyncio.run(send_message(message))
