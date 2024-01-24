609fb672f35e0f0c61306e269b70b18ebd8ff348
Николай <130489187+kolyan994@users.noreply.github.com>
2024-01-24 23:03:02 +0300

    Update echo_client.py
    v 1.1

--- echo_client.py
+++ echo_client.py
@@ -3,11 +3,14 @@ import asyncio
 
 async def send_message(message):
     reader, writer = await asyncio.open_connection('127.0.0.1', 8000)
-    writer.write(message.encode())
-    await writer.drain()
-    print(f'Отправлено: {message}')
-    data = await reader.read(100)
-    print(f'Принято: {data.decode()}')
+    for i in range(5):
+       # await asyncio.sleep(6)    # Проверка срабатывания таймаута
+        message = f"{message} {i+1}"
+        writer.write(message.encode())
+        await writer.drain()
+        print(f'Отправлено: {message}')
+        data = await reader.read(100)
+        print(f'Принято: {data.decode()}')
     writer.close()
     await writer.wait_closed()
     print('Соединение закрыто')
